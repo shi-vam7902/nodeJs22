46c67e5e4aa84587d04dfe72754a8fff60ae4c52
shi-vam7902 <shivamshah.glsbca20@gmail.com>
2022-06-18 06:00:51 +0530

    api with db

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 const express = require("express")
 const session = require("./controller/session")
+const sessionController = require("./controller/sessionController")
+const mongoose = require('mongoose');
 const app = express()
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-
+mongoose.connect("mongodb://localhost:9090/royaldb",function(err)
+{
+    if(err)
+    {
+        console.log("SomeThing went Wrong");
+        console.log(err)
+    }
+    else
+    {
+        console.log("Db connected ....");
+    }
+})
 
 app.post("/signup", session.signup)
 app.post("/login", session.login)
-
+app.post("/register",sessionController.signup)
+app.get("/getAllUsers",sessionController.getAllUsers)
 
 app.listen(9090, function () {
     console.log("Server started on 9090");
---
+++ model/userModel.js
@@ -0,0 +1,22 @@
+const mongoose =require("mongoose")
+const Schema = mongoose.Schema
+
+
+const UserSchema = new mongoose.Schema(
+    {
+        firstName : {
+            type:String
+        },
+        lastName : {
+            type:String
+        },
+        email : {
+            type:String
+        },
+        password:{
+            type:String
+        }
+    }
+)
+
+module.exports = mongoose.model("User",UserSchema)
